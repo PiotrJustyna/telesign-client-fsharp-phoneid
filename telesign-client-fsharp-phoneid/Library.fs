c5cb655753ed7d078ed4c17ccc4d46e88f2eaa1e
Piotr Justyna <piotr.justyna@gmail.com>
2022-07-27 20:48:08 +0100

    Reusing constants better.

--- telesign-client-fsharp-phoneid/Library.fs
+++ telesign-client-fsharp-phoneid/Library.fs
@@ -10,8 +10,15 @@ open System.Text
 open System.Threading.Tasks
 
 module PhoneID =
-    let stringToSign (dateRfc2616: string) (nonce: string) (resource: string) : string =
-        $"POST\napplication/json\n{dateRfc2616}\nx-ts-auth-method:HMAC-SHA256\nx-ts-nonce:{nonce}\n{{}}\n{resource}"
+    let stringToSign
+        (contentType: string)
+        (dateRfc2616: string)
+        (authorizationMethod: string)
+        (nonce: string)
+        (fieldsToSign: string)
+        (resource: string)
+        : string =
+        $"POST\n{contentType}\n{dateRfc2616}\nx-ts-auth-method:{authorizationMethod}\nx-ts-nonce:{nonce}\n{fieldsToSign}\n{resource}"
 
     let telesignAuthorizationHeader (apiKey: string) (stringToSign: string) (customerId: string) : string =
         let hasher =
@@ -42,24 +49,24 @@ module PhoneID =
         (apiKey: string)
         : HttpRequestMessage =
         let contentType = "application/json"
-        let resourceUri = $"{restEndpoint}{resource}"
-        let authMethod = "HMAC-SHA256"
-
+        let authorizationMethod = "HMAC-SHA256"
         let fieldsToSign = "{}"
+        let resourceUri = $"{restEndpoint}{resource}"
 
-        let stringToSign = stringToSign dateRfc2616 nonce resource
+        let stringToSign =
+            stringToSign contentType dateRfc2616 authorizationMethod nonce fieldsToSign resource
 
         let authorization =
             telesignAuthorizationHeader apiKey stringToSign customerId
 
         let headers =
-            telesignHeaders authorization dateRfc2616 authMethod nonce
+            telesignHeaders authorization dateRfc2616 authorizationMethod nonce
 
         let requestMessage =
             new HttpRequestMessage(HttpMethod.Post, resourceUri)
 
         requestMessage.Content <- new StringContent(fieldsToSign, Encoding.UTF8, contentType)
-        requestMessage.Content.Headers.ContentType <- MediaTypeHeaderValue("application/json")
+        requestMessage.Content.Headers.ContentType <- MediaTypeHeaderValue(contentType)
 
         for entry in headers do
             requestMessage.Headers.Add(entry.Key, entry.Value)
@@ -68,8 +75,14 @@ module PhoneID =
 
     let telesignResponse (request: HttpRequestMessage) : Task<string> =
         let client = new HttpClient()
+
         task {
             let! response = client.SendAsync(request).ConfigureAwait(false)
             let statusCode = response.StatusCode
-            return! if statusCode = HttpStatusCode.OK then response.Content.ReadAsStringAsync() else Task.FromResult String.Empty
+
+            return!
+                if statusCode = HttpStatusCode.OK then
+                    response.Content.ReadAsStringAsync()
+                else
+                    Task.FromResult String.Empty
         }
